3a11b1a190dcd5ebc2ceb76fa9e8e3ca31f3a5b1
mhendricks96 <mhendricks96@yahoo.com>
2021-09-01 00:04:23 -0700

    created app, finished list, and ability to create record. next is links on details page to delete and update

---
+++ record/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+# Create your models here.
+
+class Record(models.Model):
+  title = models.CharField(max_length=64)
+  artist = models.CharField(max_length=64)
+  genre = models.CharField(max_length=64)
+  #date_added = models.DateTimeField(auto_now_add=True)
+  added_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.title
+
+  def get_absolute_url(self):
+    return reverse('record_detail', args=(str(self.id)))
